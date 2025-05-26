732d1e42c65ce032015f135cd0e04ad32db841f2
maaru <maratarzymatov288@gmail.com>
2025-05-27 04:03:50 +0600

    fixed | select all

--- lua/plugins/misc/select_all.lua
+++ lua/plugins/misc/select_all.lua
@@ -1,40 +1,28 @@
+_G.select_all_executed = false
+
 local function select_all()
+	_G.select_all_executed = true
+
 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
 	vim.cmd("normal! ggVG")
 
-	local function restore_cursor()
-		vim.api.nvim_win_set_cursor(0, { row, col })
-	end
-
-	vim.api.nvim_create_autocmd("TextYankPost", {
-		callback = restore_cursor,
-		once = true,
-	})
+	local restore_group = vim.api.nvim_create_augroup("SelectAllRestoreGroup", { clear = true })
 
 	vim.api.nvim_create_autocmd("ModeChanged", {
-		pattern = "*:[vV\x16]*",
+		group = restore_group,
+		pattern = "*:*",
 		callback = function()
-			local mode = vim.api.nvim_get_mode().mode
-			if mode == "n" then
-				restore_cursor()
+			if _G.select_all_executed then
+				vim.api.nvim_win_set_cursor(0, { row, col })
+				_G.select_all_executed = false
+				vim.api.nvim_del_augroup_by_id(restore_group)
 			end
 		end,
 		once = true,
 	})
 
-	local keys = { "<Esc>", "<C-c>", "i", "a", "I", "A", "o", "O" }
-	for _, key in ipairs(keys) do
-		vim.api.nvim_buf_set_keymap(
-			0,
-			"v",
-			key,
-			[[<Esc>:lua (function() vim.api.nvim_win_set_cursor(0, {]] .. row .. [[, ]] .. col .. [[}) end)()<CR>]],
-			{ noremap = true, silent = true }
-		)
-	end
 	vim.cmd("normal! zz")
 end
 
-vim.api.nvim_create_user_command("SelectAll", function()
-	select_all()
-end, {})
+vim.api.nvim_create_user_command("SelectAll", select_all, {})
